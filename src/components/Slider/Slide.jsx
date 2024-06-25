7871b3f5fd7f5facda5844032dba7f37e6911539
Oleh Andrusenko <dev.andrusenko.oleh@gmail.com>
2024-06-25 13:53:20 +0300

    second version of slider. add buttons, state and auto-changing

--- src/components/Slider/Slide.jsx
+++ src/components/Slider/Slide.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Slide = ({img, title, text}) => {
     return (
         <div className='w-full h-full py-12 px-16' style={{
-            backgroundImage: `linear-gradient(to right, black, transparent), url(${img})`,
+            backgroundImage: `linear-gradient(to right,  rgba(30, 58, 138, 0.6), transparent), url(${img})`,
             backgroundPosition: 'right',
             backgroundSize: 'cover'
         }}>
             <div className='w-1/2 '>
-                <p className='text-white text-7xl font-bold mb-10'>{title}</p>
-                <p className='text-3xl text-white leading-10 font-light'>{text}</p>
+                <p className='text-white text-7xl font-bold mb-10 '>{title}</p>
+                <p className='text-2xl text-white leading-10 font-light'>{text}</p>
             </div>
         </div>
     );
